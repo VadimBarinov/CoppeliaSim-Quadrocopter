ab4bd976d9e31519089c17e7c78bbf1031298033
VadimBarinov <barinovvadim87@gmail.com>
2025-12-10 11:46:51 +0400

    refactor CoppeliaSim scene + refactor main script

--- src/main.py
+++ src/main.py
@@ -3,23 +3,23 @@ from src.core.config import settings
 
 
 def main():
-    schene_map = settings.scene_map
+    scene_map = settings.scene_map
     quadrocopter = Quadrocopter()
 
     if quadrocopter.client_id != -1:
         print("\nСоединение с сервером установлено!")
         quadrocopter.start_position(
             v_start=settings.quadrocopter.v_start,
-            scene_map=schene_map,
+            scene_map=scene_map,
         )
         while not isinstance(quadrocopter.obj_found, bool):
-            quadrocopter.search_object(schene_map)
+            quadrocopter.search_object(scene_map)
         while quadrocopter.obj_found:
-            if quadrocopter.land(schene_map):
+            if quadrocopter.land(scene_map):
                 print(quadrocopter.msg)
                 break
             else:
-                quadrocopter.search_object(schene_map)
+                quadrocopter.search_object(scene_map)
                 print(quadrocopter.msg)
         else:
             print(quadrocopter.msg)
