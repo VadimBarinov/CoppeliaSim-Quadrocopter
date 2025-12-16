655a1f6ee569526aec213a7d8f7538b6f2153ff4
VadimBarinov <barinovvadim87@gmail.com>
2025-12-16 10:54:42 +0400

    refactor scene + main.py

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def main():
         else:
             print(quadrocopter.msg)
     except:
-        print("\nНе удалось подключиться к серверу.")
+        print("\nПроизошла ошибка.")
 
 
 if __name__ == "__main__":
